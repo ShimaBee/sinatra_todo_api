8235f2209275f0dabf0449f8dcaa65bcb2597d3f
島袋雄太 <mrs1122ani@gmail.com>
2019-03-22 12:43:08 +0900

    新規登録機能

--- app.rb
+++ app.rb
@@ -2,7 +2,12 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'mysql2'
 require 'mysql2-cs-bind'
-# gem 'rack-contrib'
+require 'rack/contrib'
+require 'json'
+require 'pry'
+
+
+use Rack::PostBodyContentTypeParser
 
 get '/' do
   erb :index
@@ -17,6 +22,59 @@ def db
   )
 end
 
+
 post "/api/v1/users" do
 
+  register_data = params.to_json
+  hash = JSON.parse(register_data)
+
+  p hash
+
+  if hash['name'] == ""
+    response = {"status": 400, message: "nameを入れてください"}
+
+  elsif hash['email'] == ''
+    response = {"status": 400, message: "emailを入れてください"}
+
+  elsif hash['password'] == ''
+    response = {"status": 400, message: "passwordを入れてください"}
+
+  else
+
+    name = hash['name']
+    email = hash['email']
+    password = hash['password']
+    db.xquery('insert into User values( null, ?, null , ?, ?)', name, email, password)
+    response = {"status": 200, message: "ユーザー登録が完了しました"}
+  end
+
+  response.to_json
+
 end
+
+
+post '/api/v1/users/login' do
+
+  register_data = params.to_json
+  hash = JSON.parse(register_data)
+
+
+  # email = hash['email']
+  # password = hash['password']
+
+  if hash['email'] == ""
+    response = {"status": 400, message: "emailを入れてください"}
+
+  elsif hash['password'] == ''
+    response = {"status": 400, message: "passwordを入れてください"}
+
+  else
+
+  end
+
+  response.to_json
+end
+
+
+
+
