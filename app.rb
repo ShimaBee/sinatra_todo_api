c4903fea83310e62740b262feef92c586ec33524
島袋雄太 <mrs1122ani@gmail.com>
2019-03-23 21:04:46 +0900

    decodedekita

--- app.rb
+++ app.rb
@@ -8,7 +8,34 @@ require 'pry'
 require 'jwt'
 # require 'sinatra/json'
 
+enable :sessions
+
 use Rack::PostBodyContentTypeParser
+
+
+# module JWT
+#   def cretae_private_key
+#     @rsa_private = OpenSSL::PKey::RSA.generate(2048)
+#
+#   end
+#
+#   def encode_jwt
+#     token = JWT.encode(login_data_json, @rsa_private, 'RS256')
+#   end
+#
+#   def decode_jwt
+#     devode_token = JWT.decode(token, @rsa_private, true, { algorithm: 'RS256' })
+#   end
+#
+# end
+
+
+
+def cretare_privatekey
+  # privatekey生成
+  @rsa_private = OpenSSL::PKey::RSA.generate(2048)
+end
+
 
 def db
   @db ||= Mysql2::Client.new(
@@ -25,12 +52,15 @@ post "/api/v1/users" do
 
   if register_data['name'] == ""
     response = {"status": 400, message: "nameを入れてください"}
+    status 400
 
   elsif register_data['email'] == ''
     response = {"status": 400, message: "emailを入れてください"}
+    status 400
 
   elsif register_data['password'] == ''
     response = {"status": 400, message: "passwordを入れてください"}
+    status 400
 
   else
     name = register_data['name']
@@ -39,12 +69,14 @@ post "/api/v1/users" do
 
     if user
       response = {"status": 400, message: "すでに存在しているユーザーです"}
+      status 400
     else
       name = register_data['name']
       email = register_data['email']
       password = register_data['password']
       db.xquery('insert into User values( null, ?, null , ?, ?)', name, email, password)
       response = {"status": 200, message: "ユーザー登録が完了しました"}
+
     end
 
   end
@@ -59,9 +91,11 @@ post '/api/v1/users/login' do
 
   if login_data['email'] == ""
     response = {"status": 400, message: "emailを入れてください"}
+    status 400
 
   elsif login_data['password'] == ''
     response = {"status": 400, message: "passwordを入れてください"}
+    status 400
 
   else
     email = login_data['email']
@@ -70,23 +104,56 @@ post '/api/v1/users/login' do
 
     unless user
       response = {"status": 400, message: "存在しないユーザーです"}
+      status 400
 
     else
-      # privatekey生成
-      rsa_private = OpenSSL::PKey::RSA.generate(2048)
+      cretare_privatekey
+      # hashをjsonに戻す
       login_data_json = login_data.to_json
-      p login_data_json
       # login_dataを暗号化して、tokenにいれる。
-      token = JWT.encode(login_data_json, rsa_private, 'RS256')
+      token = JWT.encode(login_data_json, @rsa_private, 'RS256')
+      p token
+
+      p '----------------------------------'
+      devode_token = JWT.decode(token, @rsa_private, true, { algorithm: 'RS256' })
+
+      p devode_token
       response = {"status": 200, message: "ログインしました" ,"token": token}
+
     end
 
   end
+
+  @@rsa_private = @rsa_private
   response.to_json
 
 end
 
-# tokenをlogin_dataに戻す
-# devode_token = JWT.decode(token, rsa_private, true, { algorithm: 'RS256' })
-#
-# p devode_token
+get '/api/v1/users' do
+
+  users_show = db.xquery("select * from User")
+  users_show_array =  users_show.to_a
+  users_show_array.to_json
+
+end
+
+get '/api/v1/users/:id' do
+  user_ditale = db.xquery("select * from User where id = ?", params[:id]).to_a.first
+  user_ditale_array =  user_ditale
+  user_ditale_array.to_json
+
+end
+
+
+get '/api/v1/todos' do
+  token = @env['HTTP_AUTHORIZATION']
+  devode_token = JWT.decode(token, @@rsa_private, true, { algorithm: 'RS256' })
+  hash = JSON.parse(devode_token[0])
+  email = hash['email']
+  password = hash['password']
+  user = db.xquery("select * from User where email = ? and password = ?", email, password).to_a.first
+  show_user_todos = db.xquery("select * from Todo where user_id = ?", user['id']).to_a
+  show_user_todos.to_json
+
+end
+
